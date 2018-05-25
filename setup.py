dbabcc384bdae47860a62c0c5d357fa2411a8d42
nikolay-neustroev <nneustroev@riskmatch.com>
2018-05-25 13:43:28 -0400

    Bump up the artifact version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='sftpclient',
-      version='1.0.0',
+      version='1.0.1',
       description='Wrapper for some SFTP clients',
       author='erachitskiy',
       author_email='erachitskiy@pulsepoint.com',
