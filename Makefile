e5e0c6b871110a79c28c8d11cccfe51c699b8d76
Borodin Gregory <111765556+pp-gborodin@users.noreply.github.com>
2023-01-09 12:01:38 +0300

    Declare dependencies & setup GitHub actions (#3)
    * Add constraints.txt file
    
    * Update .gitignore
    
    * Setup github actions
    
    * Simplify Makefile
    
    * Use pytest instead of nose

--- Makefile
+++ Makefile
@@ -1,5 +1,22 @@
-init:
-	pip install -r requirements.txt
+SYSTEM_PYTHON := $(shell which python2)
+VENV          := venv
+PIP           := pip
 
+venv:
+	$(SYSTEM_PYTHON) -m virtualenv $(VENV)
+	$(PIP) install --upgrade pip wheel
+
+.PHONY: install
+install:
+	$(PIP) install -r requirements.txt -c constraints.txt
+	$(PIP) install -e .[test]
+
+.PHONY: test
 test:
-	nosetests tests
+	pytest tests
+
+.PHONY: constraints.txt
+constraints.txt:
+	$(PIP) freeze \
+		--exclude-editable \
+		--exclude sftpclient > constraints.txt
--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name='sftpclient',
-      version='1.0.1',
-      description='Wrapper for some SFTP clients',
-      author='erachitskiy',
-      author_email='erachitskiy@pulsepoint.com',
-      url='https://github.com/pulsepointinc/sftpclient',
-      packages=find_packages(),
-      install_requires = [
-            'ssh2-python>=0.8.0',
-            'paramiko>=2.3.1'
-      ],
-      zip_safe=False
-      )
+setup(
+    name='pp-sftpclient',
+    version='1.0.2',
+    description='Wrapper for some SFTP clients',
+    author='erachitskiy',
+    author_email='erachitskiy@pulsepoint.com',
+    url='https://github.com/pulsepointinc/sftpclient',
+    packages=find_packages(),
+    install_requires=[
+        'paramiko',
+        'sftpserver',
+        'ssh2-python',
+    ],
+    extras_require={
+        'test': [
+            'pytest',
+            'six',
+        ]
+    },
+    zip_safe=False,
+)
